1fcd6d07c1de2d671ea09c311b788c19188d9c0f
Pragmatic Software <pragma78@gmail.com>
2011-01-26 02:34:55 +0000

    config: updated example in admins file

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -13,7 +13,7 @@ use warnings;
 # These are set automatically by the build/commit script
 use constant {
   BUILD_NAME     => "PBot",
-  BUILD_REVISION => 258,
+  BUILD_REVISION => 262,
   BUILD_DATE     => "2011-01-25",
 };
 
