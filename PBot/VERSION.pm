f92da35b341b43e0a45048f92b9eb97243c28a23
Pragmatic Software <pragma78@gmail.com>
2011-01-29 08:33:00 +0000

    factoids: updated database -- moved more factoids into more namespaces

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -13,8 +13,8 @@ use warnings;
 # These are set automatically by the build/commit script
 use constant {
   BUILD_NAME     => "PBot",
-  BUILD_REVISION => 277,
-  BUILD_DATE     => "2011-01-28",
+  BUILD_REVISION => 278,
+  BUILD_DATE     => "2011-01-29",
 };
 
 1;
