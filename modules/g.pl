5e0ac12020d7ea65179c28ae5bfd9f70f19ef919
Pragmatic Software <pragma78@gmail.com>
2011-12-06 16:46:39 +0000

    Updated and reinstated google search

---
+++ modules/g.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl -w -I /home/msmud/lib/lib/perl5/site_perl/5.10.0/
+
+# Quick and dirty by :pragma
+
+use Google::Search;
+
+my ($nick, $arguments, $matches);
+
+$matches = 3;
+$nick = shift @ARGV;
+
+if ($#ARGV < 0)
+{
+  print "Usage: google [number of results] query\n";
+  die;
+}
+
+$arguments = join(" ", @ARGV);
+
+if($arguments =~ m/^([0-9]+)/)
+{
+  $matches = $1;
+  $arguments =~ s/^$1//;
+}
+
+my $search = Google::Search->Web(query => $arguments, referrer => 'http://blackshell.com');
+
+print "$nick: ";
+
+if(not $search->first) {
+    print $search->error->reason, "\n";
+    exit;
+}
+
+my $comma = "";
+while( my $result = $search->next) {
+    print $comma, $result->titleNoFormatting, ": ", $result->uri;
+    $comma = " -- ";
+    last if --$matches <= 0;
+}
+
+print "\n";
+
