016aba76dbe05ce3571bfcf44f3301a9f931b329
Pragmatic Software <pragma78@gmail.com>
2011-12-12 06:01:51 +0000

    pbot.sh: strange behavior with &> -- using 2> instead

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -13,7 +13,7 @@ use warnings;
 # These are set automatically by the build/commit script
 use constant {
   BUILD_NAME     => "PBot",
-  BUILD_REVISION => 333,
+  BUILD_REVISION => 334,
   BUILD_DATE     => "2011-12-11",
 };
 
--- pbot.sh
+++ pbot.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mv log/log log/log-`date +%Y%m%d-%H:%M.%N` &> /dev/null
-mv log/stderr_log log/stderr_log-`date +%Y%m%d-%H:%M.%N` &> /dev/null
+mv log/log log/log-`date +%Y%m%d-%H:%M.%N` 2> /dev/null
+mv log/stderr_log log/stderr_log-`date +%Y%m%d-%H:%M.%N` 2> /dev/null
 
 perl pbot.pl 2> log/stderr_log
 cat log/stderr_log
