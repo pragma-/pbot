44bef4bdb383b91cce79de8bbcf5c03bf0a38508
Pragmatic Software <pragma78@gmail.com>
2011-12-12 00:19:34 +0000

    anti-flood: check for ban evasion on nick changes

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -13,7 +13,7 @@ use warnings;
 # These are set automatically by the build/commit script
 use constant {
   BUILD_NAME     => "PBot",
-  BUILD_REVISION => 328,
+  BUILD_REVISION => 329,
   BUILD_DATE     => "2011-12-11",
 };
 
