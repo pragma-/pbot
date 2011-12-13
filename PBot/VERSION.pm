3d0f763253eb26513d65e8a9e690202f77fefa3b
Pragmatic Software <pragma78@gmail.com>
2011-12-13 04:43:57 +0000

    factoids: added hugs/hugs_response; aliased pokes, pets, spanks, slaps, hugs, etc

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -13,7 +13,7 @@ use warnings;
 # These are set automatically by the build/commit script
 use constant {
   BUILD_NAME     => "PBot",
-  BUILD_REVISION => 337,
+  BUILD_REVISION => 338,
   BUILD_DATE     => "2011-12-12",
 };
 
