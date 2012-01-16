1ba8750e0b743bb11edfeb5f22576f4e732528da
Pragmatic Software <pragma78@gmail.com>
2012-01-16 15:40:30 +0000

    Adding n1256.html and n1570.html

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -13,7 +13,7 @@ use warnings;
 # These are set automatically by the build/commit script
 use constant {
   BUILD_NAME     => "PBot",
-  BUILD_REVISION => 345,
+  BUILD_REVISION => 346,
   BUILD_DATE     => "2012-01-16",
 };
 
