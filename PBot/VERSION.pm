52b23418e2c70b0f200e48a9cba8004373752dcc
Pragmatic Software <pragma78@gmail.com>
2012-07-22 19:27:37 +0000

    Updated factoids and quotegrabs data

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -13,7 +13,7 @@ use warnings;
 # These are set automatically by the build/commit script
 use constant {
   BUILD_NAME     => "PBot",
-  BUILD_REVISION => 369,
+  BUILD_REVISION => 370,
   BUILD_DATE     => "2012-07-22",
 };
 
