f16dedf6803f76fd6756dec13e8a38fa41d65d67
Pragmatic Software <pragma78@gmail.com>
2012-10-24 12:29:00 +0000

    compiler_vm: add script to mount/umount qcow2 for editing

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -13,7 +13,7 @@ use warnings;
 # These are set automatically by the build/commit script
 use constant {
   BUILD_NAME     => "PBot",
-  BUILD_REVISION => 388,
+  BUILD_REVISION => 389,
   BUILD_DATE     => "2012-10-24",
 };
 
