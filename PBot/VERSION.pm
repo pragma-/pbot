c8d77ed2303f11e42344179c13c895b8f5aac684
Pragmatic Software <pragma78@gmail.com>
2012-11-19 15:52:07 +0000

    Replace `A-Z0-9_` in function regex with `\w` to slightly improve readability

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -13,7 +13,7 @@ use warnings;
 # These are set automatically by the build/commit script
 use constant {
   BUILD_NAME     => "PBot",
-  BUILD_REVISION => 395,
+  BUILD_REVISION => 396,
   BUILD_DATE     => "2012-11-19",
 };
 
