8afb73acc2e5bd58eef3b19edc757d61720d8e85
Pragmatic Software <pragma78@gmail.com>
2014-03-10 06:39:30 +0000

    Fix "argument isn't numeric" warning

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -13,7 +13,7 @@ use warnings;
 # These are set automatically by the build/commit script
 use constant {
   BUILD_NAME     => "PBot",
-  BUILD_REVISION => 518,
+  BUILD_REVISION => 519,
   BUILD_DATE     => "2014-03-09",
 };
 
