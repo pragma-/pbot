9bb70278b72359809b697af5bc4d5d65cc185366
Pragmatic Software <pragma78@gmail.com>
2014-06-23 11:10:40 +0000

    CGrammar: static local variables do not have internal linkage

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -13,7 +13,7 @@ use warnings;
 # These are set automatically by the build/commit script
 use constant {
   BUILD_NAME     => "PBot",
-  BUILD_REVISION => 670,
+  BUILD_REVISION => 671,
   BUILD_DATE     => "2014-06-23",
 };
 
