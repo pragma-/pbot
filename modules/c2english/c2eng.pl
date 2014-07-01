aca698374c5d8d0824c213515060f628864940e6
Pragmatic Software <pragma78@gmail.com>
2014-07-01 19:07:44 +0000

    CGrammar: Correct beautification regex for "the a"

--- modules/c2english/c2eng.pl
+++ modules/c2english/c2eng.pl
@@ -81,7 +81,7 @@ foreach my $arg (@ARGV) {
     $output =~ s/of evaluate/of/g;
     $output =~ s/the evaluate the/the/g;
     $output =~ s/by evaluate the/by the/g;
-    $output =~ s/the a/the/g;
+    $output =~ s/the a /the /g;
  
     foreach my $quote (@quotes) {
       next unless $quote;
