c638b2c9934541fa5f47c60a91a23a9fba3dae7e
Pragmatic Software <pragma78@gmail.com>
2014-07-05 00:04:15 +0000

    CGrammar: Fix shebangs to use env to detect perl location

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -13,7 +13,7 @@ use warnings;
 # These are set automatically by the build/commit script
 use constant {
   BUILD_NAME     => "PBot",
-  BUILD_REVISION => 706,
+  BUILD_REVISION => 707,
   BUILD_DATE     => "2014-07-04",
 };
 
