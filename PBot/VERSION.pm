c04896aa32903cdc01ce9a897c7f4b252cdb63c3
Pragmatic Software <pragma78@gmail.com>
2014-07-22 03:08:56 +0000

    Fix up a couple more C facts misparses

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -13,7 +13,7 @@ use warnings;
 # These are set automatically by the build/commit script
 use constant {
   BUILD_NAME     => "PBot",
-  BUILD_REVISION => 744,
+  BUILD_REVISION => 745,
   BUILD_DATE     => "2014-07-21",
 };
 
