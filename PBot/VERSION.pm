00fd1eee899194ed0c6ce4165d2f5c73c396c8ce
Pragmatic Software <pragma78@gmail.com>
2014-07-28 15:56:13 +0000

    C Jeopardy: Update question data with additional acceptable answers

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -13,7 +13,7 @@ use warnings;
 # These are set automatically by the build/commit script
 use constant {
   BUILD_NAME     => "PBot",
-  BUILD_REVISION => 752,
+  BUILD_REVISION => 753,
   BUILD_DATE     => "2014-07-28",
 };
 
