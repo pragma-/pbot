74cffe091c46e17072b57d13d687ebf082a931b8
Pragmatic Software <pragma78@gmail.com>
2014-08-04 21:51:23 +0000

    C Jeopardy: More improvements to question database

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -13,8 +13,8 @@ use warnings;
 # These are set automatically by the build/commit script
 use constant {
   BUILD_NAME     => "PBot",
-  BUILD_REVISION => 764,
-  BUILD_DATE     => "2014-08-03",
+  BUILD_REVISION => 765,
+  BUILD_DATE     => "2014-08-04",
 };
 
 1;
