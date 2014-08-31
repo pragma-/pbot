a5ede3307ea8e4d26299f0181f7facf09c4b8b83
Pragmatic Software <pragma78@gmail.com>
2014-08-31 20:23:27 +0000

    Update C facts module

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -13,7 +13,7 @@ use warnings;
 # These are set automatically by the build/commit script
 use constant {
   BUILD_NAME     => "PBot",
-  BUILD_REVISION => 779,
+  BUILD_REVISION => 780,
   BUILD_DATE     => "2014-08-31",
 };
 
--- modules/getcfact.pl
+++ modules/getcfact.pl
@@ -4,13 +4,27 @@ use warnings;
 use strict;
 
 my $CFACTS = 'cfacts.txt';
+my $CJEOPARDY_DATA = 'cjeopardy.dat';
 
 my $text = join(' ', @ARGV);
 
+sub encode { my $str = shift; $str =~ s/\\(.)/{sprintf "\\%03d", ord($1)}/ge; return $str; }
+sub decode { my $str = shift; $str =~ s/\\(\d{3})/{"\\" . chr($1)}/ge; return $str }
+
+my $jeopardy_answers;
+open my $fh, "<", $CJEOPARDY_DATA;
+if (defined $fh) {
+  $jeopardy_answers = <$fh>;
+  close $fh;
+}
+
+my @valid_answers = map { lc decode $_ } split /\|/, encode $jeopardy_answers;
+
 my @facts;
-open my $fh, "<", $CFACTS or die "Could not open $CFACTS: $!";
+open $fh, "<", $CFACTS or die "Could not open $CFACTS: $!";
 while (my $fact = <$fh>) {
   next if length $text and $fact !~ /\Q$text\E/i;
+  next if grep { $fact =~ /\Q$_\E/i } @valid_answers;
   push @facts, $fact;
 }
 close $fh;
