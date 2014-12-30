84ac99a6800408594938d9101385b25b1204f806
Pragmatic Software <pragma78@gmail.com>
2014-12-29 23:55:49 -0800

    Merge stderr into existing log file and remove stderr_log file

--- pbot.sh
+++ pbot.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
 mv log/log log/log-`date +%Y%m%d-%H:%M.%N` 2> /dev/null
-mv log/stderr_log log/stderr_log-`date +%Y%m%d-%H:%M.%N` 2> /dev/null
-
-perl pbot.pl 2> log/stderr_log
-cat log/stderr_log
+perl pbot.pl 2>> log/log
