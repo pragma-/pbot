d01cbde65b99190ab11db24080272621bcf7f1a1
Pragmatic Software <pragma78@gmail.com>
2015-04-05 02:31:45 -0700

    Add JavaScript language to compiler_vm (using Google V8)

---
+++ modules/compiler_vm/languages/javascript.pm
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+package javascript;
+use parent '_default';
+
+sub initialize {
+  my ($self, %conf) = @_;
+
+  $self->{sourcefile}      = 'prog.js';
+  $self->{execfile}        = 'prog.js';
+  $self->{default_options} = '';
+  $self->{cmdline}         = 'd8 $options $sourcefile';
+
+  $self->{cmdline_opening_comment} = "/************* CMDLINE *************\n";
+  $self->{cmdline_closing_comment} = "************** CMDLINE *************/\n";
+
+  $self->{output_opening_comment} = "/************* OUTPUT *************\n";
+  $self->{output_closing_comment} = "************** OUTPUT *************/\n";
+}
+
+1;
