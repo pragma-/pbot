8bbfe2d387e4182e9813c7a1a479dd4b6efc040f
Pragmatic Software <pragma78@gmail.com>
2015-04-06 00:49:38 -0700

    Add scheme language to compiler_vm

---
+++ modules/compiler_vm/languages/scheme.pm
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+package scheme;
+use parent '_default';
+
+sub initialize {
+  my ($self, %conf) = @_;
+
+  $self->{sourcefile}      = 'prog.scm';
+  $self->{execfile}        = 'prog.scm';
+  $self->{default_options} = '';
+  $self->{cmdline}         = 'scm $options < $sourcefile';
+
+  $self->{cmdline_opening_comment} = "#|=============== CMDLINE ===============\n";
+  $self->{cmdline_closing_comment} = "================= CMDLINE ===============|#\n";
+
+  $self->{output_opening_comment} = "#|=============== OUTPUT ===============\n";
+  $self->{output_closing_comment} = "================= OUTPUT ===============|#\n";
+}
+
+sub postprocess_output {
+  my $self = shift;
+  $self->SUPER::postprocess_output;
+
+  $self->{output} =~ s/^\n//;
+}
+
+1;
