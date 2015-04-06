332545452f6da1155663f7d50de04d18006931f5
Pragmatic Software <pragma78@gmail.com>
2015-04-06 00:49:29 -0700

    Add clisp language to compiler_vm

---
+++ modules/compiler_vm/languages/clisp.pm
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+package clisp;
+use parent '_default';
+
+sub initialize {
+  my ($self, %conf) = @_;
+
+  $self->{sourcefile}      = 'prog.lisp';
+  $self->{execfile}        = 'prog.lisp';
+  $self->{default_options} = '';
+  $self->{cmdline}         = 'clisp $options $sourcefile';
+
+  $self->{sprunge_lexer}   = 'cl';
+
+  $self->{cmdline_opening_comment} = "#|=============== CMDLINE ===============\n";
+  $self->{cmdline_closing_comment} = "================= CMDLINE ===============|#\n";
+
+  $self->{output_opening_comment} = "#|=============== OUTPUT ===============\n";
+  $self->{output_closing_comment} = "================= OUTPUT ===============|#\n";
+}
+
+sub postprocess_output {
+  my $self = shift;
+  $self->SUPER::postprocess_output;
+
+  $self->{output} =~ s/^\n//;
+}
+
+1;
