a76713a6123fac8f616b5c2886d406424fb34fd9
Pragmatic Software <pragma78@gmail.com>
2017-08-03 13:29:20 -0700

    Match channel exactly in blacklist

--- PBot/BlackList.pm
+++ PBot/BlackList.pm
@@ -153,7 +153,7 @@ sub check_blacklist {
       } elsif ($flag eq 'r' && defined $gecos && $gecos =~ /$black_hostmask_escaped/i) {
         $self->{pbot}->{logger}->log("$hostmask GECOS $gecos blacklisted in channel $channel (matches [\$r:$black_hostmask] host and [$black_channel] channel)\n");
         return 1;
-      } elsif ($flag eq '' && $channel =~ /$black_channel_escaped/i && $hostmask =~ /$black_hostmask_escaped/i) {
+      } elsif ($flag eq '' && $channel =~ /^$black_channel_escaped$/i && $hostmask =~ /$black_hostmask_escaped/i) {
         $self->{pbot}->{logger}->log("$hostmask blacklisted in channel $channel (matches [$black_hostmask] host and [$black_channel] channel)\n");
         return 1;
       }
