e82e4b15a0b97640c305693f281a479c7081fe87
alyptik <alyptik@protonmail.com>
2017-09-02 20:27:27 -1000

    RandomFact.pm: add random factoid command

---
+++ PBot/Plugins/RandomFact.pm
@@ -0,0 +1,48 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package PBot::Plugins::RandomFact;
+
+use warnings;
+use strict;
+
+use Carp ();
+
+sub new {
+  Carp::croak("Options to " . __FILE__ . " should be key/value pairs, not hash reference") if ref $_[1] eq 'HASH';
+  my ($class, %conf) = @_;
+  my $self = bless {}, $class;
+  $self->initialize(%conf);
+  return $self;
+}
+
+sub initialize {
+  my ($self, %conf) = @_;
+  $self->{pbot} = delete $conf{pbot} // Carp::croak("Missing pbot reference to " . __FILE__);
+  $self->{pbot}->{commands}->register(sub { $self->rand_factoid(@_) }, 'rfact', 0);
+}
+
+sub unload {
+  my $self = shift;
+  $self->{pbot}->{commands}->unregister('rfact');
+}
+
+sub rand_factoid {
+  my $self = shift;
+  my ($channel, $args) = @_;
+
+  # pick random channel unless given one
+  my @channels = keys %{ $self->{pbot}->{factoids}->hash } unless defined $channel;
+  $channel = @channels[int rand @channels] unless defined $channel;
+  # pick random trigger
+  my @triggers = keys %{ $self->{pbot}->{triggers}->hash->{$channel} };
+  my $trigger = @triggers[int rand @triggers];
+  # populate other vars
+  my $owner = $self->{factoids}->hash->{$channel}->{$trigger}->{owner};
+  my $action = $self->{factoids}->hash->{$channel}->{$trigger}->{action};
+
+  return "[$channel] $trigger is “$action” (created by $owner)";
+}
+
+1;
