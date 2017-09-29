fc18d8a4a6a9bcde4888f8ac94e472d3c0a7fed4
alyptik <alyptik@protonmail.com>
2017-09-28 20:38:10 -1000

    RandomFact.pm: add state variables

--- PBot/Plugins/RandomFact.pm
+++ PBot/Plugins/RandomFact.pm
@@ -6,16 +6,30 @@
 
 package PBot::Plugins::RandomFact;
 
+use feature qw/state/;
 use warnings;
 use strict;
 
 use Carp ();
+use Time::HiRes qw/gettimeofday/;
+use Time::Duration;
 
 sub new {
   Carp::croak("Options to " . __FILE__ . " should be key/value pairs, not hash reference") if ref $_[1] eq 'HASH';
   my ($class, %conf) = @_;
   my $self = bless {}, $class;
+  state ($last_regen, $time_of_death, $kneads_until_death);
+
   $self->initialize(%conf);
+  $self->{pbot}->{states}->{randomfact} = {
+    last_regen => $last_regen,
+    time_of_death => $time_of_death,
+    kneads_until_death => $kneads_until_death,
+  };
+  $self->{pbot}->{states}->{randomfact}->{last_regen} = gettimeofday;
+  $self->{pbot}->{states}->{randomfact}->{time_of_death} = gettimeofday;
+  $self->{pbot}->{states}->{randomfact}->{kneads_until_death} = $self->{pbot}->{registry}->get_value('randomfact', 'knead_max');
+
   return $self;
 }
 
@@ -25,8 +39,11 @@ sub initialize {
   $self->{pbot} = delete $conf{pbot} // Carp::croak("Missing pbot reference to " . __FILE__);
   $self->{pbot}->{commands}->register(sub { $self->rand_factoid(@_) }, 'rfact', 0);
 
-  $self->{pbot}->{registry}->add_default('text',  'randomfact', 'savageosity',  '2');
-  $self->{pbot}->{registry}->add_default('text',  'randomfact', 'kneadoreshold',  '3');
+  $self->{pbot}->{registry}->add_default('text', 'randomfact', 'savageosity',  '1');
+  $self->{pbot}->{registry}->add_default('text', 'randomfact', 'hp_regen',  '30');
+  $self->{pbot}->{registry}->add_default('text', 'randomfact', 'knead_max', '6');
+
+  $self->{pbot}->{event_dispatcher}->register_handler('irc.kick', sub { $self->on_kick(@_) });
 }
 
 sub unload {
