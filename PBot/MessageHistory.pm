928c7d2cbf8a98ff617e10fbc5136a67521295ae
alyptik <alyptik@protonmail.com>
2017-11-11 16:27:52 -1000

    MessageHistory.pm: make quit messages sexy

--- PBot/MessageHistory.pm
+++ PBot/MessageHistory.pm
@@ -398,6 +398,8 @@ sub recall_message {
       if ($text =~ s/^(NICKCHANGE)\b/changed nick to/ or
           $text =~ s/^(KICKED|QUIT)\b/lc "$1"/e or
           $text =~ s/^(JOIN|PART)\b/lc "$1ed"/e) {
+        # fix ugly "[nick] quit Quit: Leaving." messages
+        $text =~ s/^(quit) (.*)/$1 ($2)/;
         $recall_text .= "[$ago] $msg->{nick} $text\n";
       } elsif ($text =~ s/^\/me\s+//) {
         $recall_text .= "[$ago] * $msg->{nick} $text\n";
