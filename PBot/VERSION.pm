428be984a2c6a08e534a4beb24f1277d9291d4e9
Pragmatic Software <pragma78@gmail.com>
2018-01-19 10:46:19 -0800

    Update version

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -17,8 +17,8 @@ use warnings;
 # These are set automatically by the build/commit script
 use constant {
   BUILD_NAME     => "PBot",
-  BUILD_REVISION => 1931,
-  BUILD_DATE     => "2018-01-17",
+  BUILD_REVISION => 1933,
+  BUILD_DATE     => "2018-01-19",
 };
 
 1;
