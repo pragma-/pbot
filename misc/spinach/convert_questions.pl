d70fa551743eb4e8fc9704d78752d43abaf29aff
Pragmatic Software <pragma78@gmail.com>
2018-02-18 18:33:07 -0800

    Remove unnecessary blank lines

--- misc/spinach/convert_questions.pl
+++ misc/spinach/convert_questions.pl
@@ -2,11 +2,9 @@
 
 use warnings;
 use strict;
-
 use JSON;
 
 my $file = 'cat_questions';
-
 open my $fh, '<', $file or die "$file: $!";
 
 my $q = { questions => [] };
@@ -15,17 +13,12 @@ my $id = 0;
 foreach my $line (<$fh>) {
   chomp $line;
   print STDERR "<$line>\n";
-
   $id++;
-  
   my ($category, $question, @answers) = split /`/, $line;
   my $answer = shift @answers;
-
   my $h = { category => $category, question => $question, answer => $answer, alternativeSpellings => \@answers, suggestions => [], id => $id };
-
   push @{$q->{questions}}, $h;
 }
 
 my $json = encode_json $q;
-
 print "$json\n";
