50d859cb0bf2b785ca21edc2735a4351702670e2
Pragmatic Software <pragma78@gmail.com>
2018-07-31 17:21:37 -0700

    Remove annoying log message

--- PBot/BanTracker.pm
+++ PBot/BanTracker.pm
@@ -166,7 +166,7 @@ sub get_baninfo {
         $baninfo->{when} = $self->{banlist}->{$channel}->{$mode}->{$banmask_key}->[1];
         $baninfo->{type} = $mode;
         #$self->{pbot}->{logger}->log("get-baninfo: dump: " . Dumper($baninfo) . "\n");
-        $self->{pbot}->{logger}->log("get-baninfo: $baninfo->{banmask} $baninfo->{type} in $baninfo->{channel} by $baninfo->{owner} on $baninfo->{when}\n");
+        #$self->{pbot}->{logger}->log("get-baninfo: $baninfo->{banmask} $baninfo->{type} in $baninfo->{channel} by $baninfo->{owner} on $baninfo->{when}\n");
 
         push @$bans, $baninfo;
       }
