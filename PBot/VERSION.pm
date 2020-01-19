d14ab77764116c5aaf19a725d8b4636c0fdd4026
Pragmatic Software <pragma78@gmail.com>
2020-01-18 22:13:47 -0800

    Update version

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -25,7 +25,7 @@ BEGIN {
 # These are set automatically by the build/commit script
 use constant {
   BUILD_NAME     => "PBot",
-  BUILD_REVISION => 2823,
+  BUILD_REVISION => 2827,
   BUILD_DATE     => "2020-01-18",
 };
 
