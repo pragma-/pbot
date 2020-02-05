61f13f6b4054a2448adedff54994711f3c6ba2de
Pragmatic Software <pragma78@gmail.com>
2020-02-05 01:41:09 -0800

    IRCHandlers: remove trailing space when setting mode without target

--- PBot/IRCHandlers.pm
+++ PBot/IRCHandlers.pm
@@ -243,7 +243,7 @@ sub on_mode {
     $mode_char = $char;
     $target = $event->{event}->{args}[++$i];
 
-    $self->{pbot}->{logger}->log("Mode $channel [$mode $target] by $nick!$user\@$host\n");
+    $self->{pbot}->{logger}->log("Mode $channel [$mode" . (length $target ? " $target" : '') . "] by $nick!$user\@$host\n");
 
     if ($mode eq "-b" or $mode eq "+b" or $mode eq "-q" or $mode eq "+q") {
       $self->{pbot}->{bantracker}->track_mode("$nick!$user\@$host", $mode, $target, $channel);
