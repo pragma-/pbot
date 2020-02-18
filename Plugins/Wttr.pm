e0fa65f0038f2555023690a946d5192098336cbd
Pragmatic Software <pragma78@gmail.com>
2020-02-18 15:06:26 -0800

    Plugins/Wttr: OCD formatting?

--- Plugins/Wttr.pm
+++ Plugins/Wttr.pm
@@ -212,16 +212,16 @@ sub get_wttr {
 
             when ('chances') {
                 $result .= "Chances of: ";
-                $result .= "Fog: $h->{'chanceoffog'}%, " if $h->{'chanceoffog'};
-                $result .= "Frost: $h->{'chanceoffrost'}%, " if $h->{'chanceoffrost'};
-                $result .= "High temp: $h->{'chanceofhightemp'}%, " if $h->{'chanceofhightemp'};
-                $result .= "Overcast: $h->{'chanceofovercast'}%, " if $h->{'chanceofovercast'};
-                $result .= "Rain: $h->{'chanceofrain'}%, " if $h->{'chanceofrain'};
-                $result .= "Remaining dry: $h->{'chanceofremdry'}%, " if $h->{'chanceofremdry'};
-                $result .= "Snow: $h->{'chanceofsnow'}%, " if $h->{'chanceofsnow'};
-                $result .= "Sunshine: $h->{'chanceofsunshine'}%, " if $h->{'chanceofsunshine'};
-                $result .= "Thunder: $h->{'chanceofthunder'}%, " if $h->{'chanceofthunder'};
-                $result .= "Windy: $h->{'chanceofwindy'}%, " if $h->{'chanceofwindy'};
+                $result .= "Fog: "           . $h->{'chanceoffog'}      . "%, "  if $h->{'chanceoffog'};
+                $result .= "Frost: "         . $h->{'chanceoffrost'}    . "%, "  if $h->{'chanceoffrost'};
+                $result .= "High temp: "     . $h->{'chanceofhightemp'} . "%, "  if $h->{'chanceofhightemp'};
+                $result .= "Overcast: "      . $h->{'chanceofovercast'} . "%, "  if $h->{'chanceofovercast'};
+                $result .= "Rain: "          . $h->{'chanceofrain'}     . "%, "  if $h->{'chanceofrain'};
+                $result .= "Remaining dry: " . $h->{'chanceofremdry'}   . "%, "  if $h->{'chanceofremdry'};
+                $result .= "Snow: "          . $h->{'chanceofsnow'}     . "%, "  if $h->{'chanceofsnow'};
+                $result .= "Sunshine: "      . $h->{'chanceofsunshine'} . "%, "  if $h->{'chanceofsunshine'};
+                $result .= "Thunder: "       . $h->{'chanceofthunder'}  . "%, "  if $h->{'chanceofthunder'};
+                $result .= "Windy: "         . $h->{'chanceofwindy'}    . "%, "  if $h->{'chanceofwindy'};
                 $result =~ s/,\s+$/; /;
             }
 
