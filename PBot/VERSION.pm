fc648c3351ac032214f50971ec4642f50288a1ec
Pragmatic Software <pragma78@gmail.com>
2020-07-06 20:56:08 -0700

    Update version

--- PBot/VERSION.pm
+++ PBot/VERSION.pm
@@ -19,8 +19,8 @@ use LWP::UserAgent;
 # These are set automatically by the misc/update_version script
 use constant {
     BUILD_NAME     => "PBot",
-    BUILD_REVISION => 3772,
-    BUILD_DATE     => "2020-07-02",
+    BUILD_REVISION => 3775,
+    BUILD_DATE     => "2020-07-06",
 };
 
 sub initialize {
