f8c73d4f62454a41e688c4098321ff10e5e36d39
Pragmatic Software <pragma78@gmail.com>
2021-05-29 14:00:22 -0700

    DualIndexSQLiteObject: get_each(): fix expression splitting

--- PBot/DualIndexSQLiteObject.pm
+++ PBot/DualIndexSQLiteObject.pm
@@ -350,7 +350,7 @@ sub get_each {
         my $everything = 0;
 
         foreach my $expr (@opts) {
-            my ($key, $op, $value) = split /\s*([!=<>]+)\s*/, $expr, 3;
+            my ($key, $op, $value) = split /\s+/, $expr, 3;
 
             if ($key eq '_everything') {
                 $everything = 1;
