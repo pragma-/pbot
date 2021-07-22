b2660352fd7747d402e74e78147cdfae35a8636a
Pragmatic Software <pragma78@gmail.com>
2021-07-22 05:36:56 -0700

    Update version

--- lib/PBot/VERSION.pm
+++ lib/PBot/VERSION.pm
@@ -23,7 +23,7 @@ our @EXPORT_OK = (
 # These are set automatically by the misc/update_version script
 use constant {
     BUILD_NAME     => "PBot",
-    BUILD_REVISION => 4269,
+    BUILD_REVISION => 4271,
     BUILD_DATE     => "2021-07-22",
 };
 
