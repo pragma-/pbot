4cf1e7616920e5240d37e826ea6f5a0ce0b2286d
Pragmatic Software <pragma78@gmail.com>
2021-10-19 21:14:52 -0700

    Update commands and factoids

--- lib/PBot/VERSION.pm
+++ lib/PBot/VERSION.pm
@@ -25,8 +25,8 @@ use PBot::Imports;
 # These are set by the /misc/update_version script
 use constant {
     BUILD_NAME     => "PBot",
-    BUILD_REVISION => 4406,
-    BUILD_DATE     => "2021-09-17",
+    BUILD_REVISION => 4415,
+    BUILD_DATE     => "2021-10-19",
 };
 
 sub initialize {}
