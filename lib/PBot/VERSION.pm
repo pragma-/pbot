5bc47b61d9ca368cebedd35fb20017df24b128c8
Pragmatic Software <pragma78@gmail.com>
2022-04-04 21:54:39 -0700

    pbot-vm: display escaped malformed Unicode in output instead of U+FFFD

--- lib/PBot/VERSION.pm
+++ lib/PBot/VERSION.pm
@@ -25,7 +25,7 @@ use PBot::Imports;
 # These are set by the /misc/update_version script
 use constant {
     BUILD_NAME     => "PBot",
-    BUILD_REVISION => 4523,
+    BUILD_REVISION => 4524,
     BUILD_DATE     => "2022-04-04",
 };
 
