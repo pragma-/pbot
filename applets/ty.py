acbebddaccd1a7f495ce7af0701a417f5d3e9393
Pragmatic Software <pragma78@gmail.com>
2022-04-08 09:10:07 -0700

    applets/ty.py: URL-encode form data

--- applets/ty.py
+++ applets/ty.py
@@ -6,11 +6,17 @@ import subprocess
 url = 'https://based.lol/run.ty'
 
 if len(sys.argv) <= 1:
-    print('Usage: ty` <code> [-stdin=<input>]')
+    print('usage: ty` <code> [-stdin=<input>]')
     sys.exit(0)
 
 cmd = ' '.join(sys.argv[1:]).replace('\\n', '\n')
 
 [code, *input] = cmd.split('-stdin=')
 
-print(subprocess.run(['curl', url, '-F', f'code={code}', '-F', f'input={"".join(input)}'], text=True, capture_output=True).stdout.rstrip())
+print(
+    subprocess.run([
+        'curl', url,
+        '--data-urlencode', f'code={code}',
+        '--data-urlencode', f'input={"".join(input)}'
+    ], text=True, capture_output=True).stdout.rstrip()
+)
