10e49bbbb67858ec27a9eadf9ca81b62cedcbe8b
Pragmatic Software <pragma78@gmail.com>
2022-07-08 12:50:30 -0700

    Update factoids

--- lib/PBot/VERSION.pm
+++ lib/PBot/VERSION.pm
@@ -25,7 +25,7 @@ use PBot::Imports;
 # These are set by the /misc/update_version script
 use constant {
     BUILD_NAME     => "PBot",
-    BUILD_REVISION => 4554,
+    BUILD_REVISION => 4555,
     BUILD_DATE     => "2022-07-08",
 };
 
