62849c9cc4f9a06258f8237cd2c7e7079f3013d5
Pragmatic Software <pragma78@gmail.com>
2022-08-03 11:31:09 -0700

    Bump version for previous revert

--- lib/PBot/VERSION.pm
+++ lib/PBot/VERSION.pm
@@ -25,7 +25,7 @@ use PBot::Imports;
 # These are set by the /misc/update_version script
 use constant {
     BUILD_NAME     => "PBot",
-    BUILD_REVISION => 4561,
+    BUILD_REVISION => 4564,
     BUILD_DATE     => "2022-08-03",
 };
 
