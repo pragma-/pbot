9ddd38038ac2b77693d7faea7f865e82fbaeccf1
Pragmatic Software <pragma78@gmail.com>
2022-08-07 19:10:34 -0700

    applets: add C23 draft (n3047) command
    I really, really need to clean this mess up and combine
    c99std.pl, c11std.pl and c23std.pl into one script!

--- lib/PBot/VERSION.pm
+++ lib/PBot/VERSION.pm
@@ -25,7 +25,7 @@ use PBot::Imports;
 # These are set by the /misc/update_version script
 use constant {
     BUILD_NAME     => "PBot",
-    BUILD_REVISION => 4565,
+    BUILD_REVISION => 4566,
     BUILD_DATE     => "2022-08-07",
 };
 
