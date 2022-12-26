ad449ff07b19197198c6cd15a7095c8234d169c7
Pragmatic Software <pragma78@gmail.com>
2022-12-25 18:22:21 -0800

    Update version

--- lib/PBot/VERSION.pm
+++ lib/PBot/VERSION.pm
@@ -25,8 +25,8 @@ use PBot::Imports;
 # These are set by the /misc/update_version script
 use constant {
     BUILD_NAME     => "PBot",
-    BUILD_REVISION => 4590,
-    BUILD_DATE     => "2022-10-01",
+    BUILD_REVISION => 4597,
+    BUILD_DATE     => "2022-12-25",
 };
 
 sub initialize {}
