224a00949a322e569714b2023d2f768be3de6572
Pragmatic Software <pragma78@gmail.com>
2023-02-09 13:39:10 -0800

    pbot-vm: guest-gdb: fix potential undefined `$start` when optimizations are enabled

--- lib/PBot/VERSION.pm
+++ lib/PBot/VERSION.pm
@@ -25,8 +25,8 @@ use PBot::Imports;
 # These are set by the /misc/update_version script
 use constant {
     BUILD_NAME     => "PBot",
-    BUILD_REVISION => 4616,
-    BUILD_DATE     => "2023-02-02",
+    BUILD_REVISION => 4617,
+    BUILD_DATE     => "2023-02-09",
 };
 
 sub initialize {}
