7da7883d9da0da31b6b82aa55656980873d6dacd
Pragmatic Software <pragma78@gmail.com>
2023-02-09 21:04:30 -0800

    pbot-vm: guest-gdb: stricter match for main()

--- lib/PBot/VERSION.pm
+++ lib/PBot/VERSION.pm
@@ -25,7 +25,7 @@ use PBot::Imports;
 # These are set by the /misc/update_version script
 use constant {
     BUILD_NAME     => "PBot",
-    BUILD_REVISION => 4617,
+    BUILD_REVISION => 4618,
     BUILD_DATE     => "2023-02-09",
 };
 
