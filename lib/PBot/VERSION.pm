00077e0eefa2071479796b276fcda8ac09b1f0ee
Pragmatic Software <pragma78@gmail.com>
2023-03-19 14:23:27 -0700

    applets/urban: fix yet another UTF-8 issue

--- lib/PBot/VERSION.pm
+++ lib/PBot/VERSION.pm
@@ -25,8 +25,8 @@ use PBot::Imports;
 # These are set by the /misc/update_version script
 use constant {
     BUILD_NAME     => "PBot",
-    BUILD_REVISION => 4630,
-    BUILD_DATE     => "2023-03-13",
+    BUILD_REVISION => 4631,
+    BUILD_DATE     => "2023-03-19",
 };
 
 sub initialize {}
