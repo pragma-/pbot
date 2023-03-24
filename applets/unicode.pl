bfabf3e3088a453f09f34bf280ca4205a4d5b77b
Pragmatic Software <pragma78@gmail.com>
2023-03-23 17:15:19 -0700

    Add applets/unicode.pl
    A quick-and-dirty simplified interface to https://github.com/garabik/unicode
    with support for searching by name/description and querying by character
    or U+XXXX code-point.

---
+++ applets/unicode.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+
+# SPDX-FileCopyrightText: 2023 Pragmatic Software <pragma78@gmail.com>
+# SPDX-License-Identifier: MIT
+
+# quick-and-dirty
+
+use warnings;
+use strict;
+
+use Encode;
+
+if (not @ARGV) {
+    print "Usage: unicode <expression>\n";
+    exit;
+}
+
+@ARGV = map { decode('UTF-8', $_, 1) } @ARGV;
+
+my $args = join ' ', @ARGV;
+
+my $search = 0;
+
+if ($args =~ s/^-s\s+// || length $args > 1) {
+    $search = 1;
+}
+
+my $result;
+
+if ($args =~ /^u\+/i) {
+    $result = `unicode --color=off \Q$args\E`;
+} elsif ($search) {
+    $result = `unicode -r --color=off \Q$args\E --format 'U+{ordc:04X} {pchar} {name}\n'`;
+} else {
+    $result = `unicode -s --color=off \Q$args\E`;
+}
+
+$result = join '; ', split /\n/, $result;
+
+print "$result\n";
