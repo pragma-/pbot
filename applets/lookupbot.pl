19f53cde10dfb1bf836ff28b8cadecf4f8bab201
Pragmatic Software <pragma78@gmail.com>
2024-03-10 12:34:42 -0700

    applets/lookupbot.pl: remove -T taint mode (breaks local::lib)

--- applets/lookupbot.pl
+++ applets/lookupbot.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -T
+#!/usr/bin/perl
 
 use strict;
 use LWP::Simple;
--- lib/PBot/VERSION.pm
+++ lib/PBot/VERSION.pm
@@ -25,8 +25,8 @@ use PBot::Imports;
 # These are set by the /misc/update_version script
 use constant {
     BUILD_NAME     => "PBot",
-    BUILD_REVISION => 4715,
-    BUILD_DATE     => "2024-03-09",
+    BUILD_REVISION => 4716,
+    BUILD_DATE     => "2024-03-10",
 };
 
 sub initialize {}
