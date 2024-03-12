ef26b39658480cca98e5cdda39dd2477f421cd56
Alex Belanger <nitrix@nitrix.me>
2024-03-12 01:32:44 -0400

    Wrong cwd for the docker server, it needs to invoke vm-exec.

--- applets/pbot-vm/entrypoint.sh
+++ applets/pbot-vm/entrypoint.sh
@@ -3,5 +3,5 @@
 socat -d -d pty,raw,link=/tmp/ttyS2,echo=0 pty,raw,link=/tmp/ttyS2x,echo=0 &
 socat -d -d pty,raw,link=/tmp/ttyS3,echo=0 pty,raw,link=/tmp/ttyS3x,echo=0 &
 
-/opt/pbot/applets/pbot-vm/host/bin/docker-server &
+cd /opt/pbot/applets/pbot-vm/host/bin && ./docker-server &
 guest-server
