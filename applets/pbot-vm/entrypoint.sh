d810f67bc52214fdfa47209b0b17f40dadeaa633
Alex Belanger <nitrix@nitrix.me>
2024-03-12 03:16:03 -0400

    How about now?

--- applets/pbot-vm/entrypoint.sh
+++ applets/pbot-vm/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-socat -d -d pty,raw,link=/tmp/ttyS2,echo=0 pty,raw,link=/tmp/ttyS2x,echo=0 &
-socat -d -d pty,raw,link=/tmp/ttyS3,echo=0 pty,raw,link=/tmp/ttyS3x,echo=0 &
+# socat -d -d pty,raw,link=/tmp/ttyS2,echo=0 pty,raw,link=/tmp/ttyS2x,echo=0 &
+# socat -d -d pty,raw,link=/tmp/ttyS3,echo=0 pty,raw,link=/tmp/ttyS3x,echo=0 &
 
-cd /opt/pbot/applets/pbot-vm/host/bin && ./docker-server &
-guest-server
+cd /opt/pbot/applets/pbot-vm/guest/bin && ./guest-server &
+cd /opt/pbot/applets/pbot-vm/host/bin && ./docker-server
