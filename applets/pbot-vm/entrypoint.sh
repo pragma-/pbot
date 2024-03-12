a869f0edcaf8b23b2d2c6b04ba6d6593d92e7dd1
Alex Belanger <nitrix@nitrix.me>
2024-03-12 01:20:42 -0400

    Wrong servers running, need to skip heartbeat.

--- applets/pbot-vm/entrypoint.sh
+++ applets/pbot-vm/entrypoint.sh
@@ -3,6 +3,5 @@
 socat -d -d pty,raw,link=/tmp/ttyS2,echo=0 pty,raw,link=/tmp/ttyS2x,echo=0 &
 socat -d -d pty,raw,link=/tmp/ttyS3,echo=0 pty,raw,link=/tmp/ttyS3x,echo=0 &
 
-guest-server &
 /opt/pbot/applets/pbot-vm/host/bin/docker-server &
-/opt/pbot/applets/pbot-vm/host/bin/vm-server
+guest-server
