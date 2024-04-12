111f3bd2e7fff01a4fe36388eaaedf938296aece
Pragmatic Software <pragma78@gmail.com>
2024-04-12 13:07:27 -0700

    pbot-vm: consolidate network enable/disable into one script

--- lib/PBot/VERSION.pm
+++ lib/PBot/VERSION.pm
@@ -25,8 +25,8 @@ use PBot::Imports;
 # These are set by the /misc/update_version script
 use constant {
     BUILD_NAME     => "PBot",
-    BUILD_REVISION => 4747,
-    BUILD_DATE     => "2024-04-10",
+    BUILD_REVISION => 4748,
+    BUILD_DATE     => "2024-04-12",
 };
 
 sub initialize {}
