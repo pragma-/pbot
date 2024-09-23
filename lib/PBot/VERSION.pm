50ca5b57f618c31fe37cc3e468169284bc2a16d3
Pragmatic Software <pragma78@gmail.com>
2024-09-23 11:27:04 -0700

    Remove TypoSub from plugin_autoload

--- lib/PBot/VERSION.pm
+++ lib/PBot/VERSION.pm
@@ -25,8 +25,8 @@ use PBot::Imports;
 # These are set by the /misc/update_version script
 use constant {
     BUILD_NAME     => "PBot",
-    BUILD_REVISION => 4789,
-    BUILD_DATE     => "2024-09-22",
+    BUILD_REVISION => 4790,
+    BUILD_DATE     => "2024-09-23",
 };
 
 sub initialize {}
