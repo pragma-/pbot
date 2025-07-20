9d29336cb28ca5f7ebf9016917757707086ceda4
Pragmatic Software <pragma78@gmail.com>
2025-07-20 00:58:37 -0700

    applets/qalc.pl: make qalc safe

--- applets/qalc.pl
+++ applets/qalc.pl
@@ -11,6 +11,18 @@ if (not @ARGV) {
     exit;
 }
 
+my $is_safe = `qalc export`;
+
+if ($is_safe ne "export() = 0\n") {
+    system("./qalc-safe > /dev/null");
+
+    $is_safe = `qalc export`;
+    if ($is_safe ne "export() = 0\n") {
+        print "Fatal: Unable to make qalc safe. Execute `qalc-safe` and check for errors.\n";
+        exit;
+    }
+}
+
 my $result = `ulimit -t 2; qalc \Q@ARGV\E`;
 
 $result =~ s/^.*approx.\s+//;
--- lib/PBot/VERSION.pm
+++ lib/PBot/VERSION.pm
@@ -25,8 +25,8 @@ use PBot::Imports;
 # These are set by the /misc/update_version script
 use constant {
     BUILD_NAME     => "PBot",
-    BUILD_REVISION => 4875,
-    BUILD_DATE     => "2025-07-19",
+    BUILD_REVISION => 4876,
+    BUILD_DATE     => "2025-07-20",
 };
 
 sub initialize {}
