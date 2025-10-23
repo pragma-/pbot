e2e153b50b889a0482ad12291dfd7ab9abe44ef1
Pragmatic Software <pragma78@gmail.com>
2025-10-22 19:42:51 -0700

    Core/Utils: add isabbrev() subroutine

---
+++ lib/PBot/Core/Utils/IsAbbrev.pm
@@ -0,0 +1,20 @@
+# File: IsAbbrev.pm
+#
+# Purpose: Check is a string is an abbreviation of another string.
+
+# SPDX-FileCopyrightText: 2017-2023 Pragmatic Software <pragma78@gmail.com>
+# SPDX-License-Identifier: MIT
+
+package PBot::Core::Utils::IsAbbrev;
+
+use PBot::Imports;
+
+require Exporter;
+our @ISA    = qw/Exporter/;
+our @EXPORT = qw/isabbrev/;
+
+sub isabbrev($str1, $str2) {
+    return (substr($str1, 0, length $str1) eq substr($str2, 0, length $str1));
+}
+
+1;
